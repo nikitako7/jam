fa2c427d3b395284ce92702cf2c2cfbad4600a5a
mkonovalov <mkonovalov@griddynamics.com>
2022-04-23 17:40:32 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: "778twsnizr4w",
+    accessToken: "WiDRNGHInpHEJqkolz91aQRBcsaQVop2tB8tHFUpYLs",
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
     </div>
-  )
+  );
 }
